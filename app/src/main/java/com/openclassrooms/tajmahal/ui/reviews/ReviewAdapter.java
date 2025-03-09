a951b37a7252c045c87abfed477bd4fe9534a658
MARC NAVARRO <navamarc@gmail.com>
2025-03-09 18:04:57 +0100

    Integration ShapeableImageView sur reviews.xml et fragment_reviews.xml, Suppressions de fichiers inutiles, Modifications de ReviewAdapter pour ShapeableImageView

--- app/src/main/java/com/openclassrooms/tajmahal/ui/reviews/ReviewAdapter.java
+++ app/src/main/java/com/openclassrooms/tajmahal/ui/reviews/ReviewAdapter.java
@@ -12,11 +12,14 @@ package com.openclassrooms.tajmahal.ui.reviews;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.google.android.material.imageview.ShapeableImageView;
 import com.openclassrooms.tajmahal.R;
 import com.openclassrooms.tajmahal.domain.model.Review;
 
@@ -39,6 +42,9 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         Review review = reviewList.get(position);
+        Glide.with(holder.itemView.getContext())
+            .load(review.getPicture())
+            .into(holder.reviewPicture);
         holder.reviewName.setText(review.getUsername());
         holder.reviewBar.setRating(review.getRate());
         holder.reviewText.setText(review.getComment());
@@ -52,12 +58,14 @@ public class ReviewAdapter extends RecyclerView.Adapter<ReviewAdapter.ViewHolder
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView reviewName, reviewText;
         RatingBar reviewBar;
+        ShapeableImageView reviewPicture;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             reviewName = itemView.findViewById(R.id.reviewName);
             reviewBar = itemView.findViewById(R.id.reviewBar);
             reviewText = itemView.findViewById(R.id.reviewText);
+            reviewPicture = itemView.findViewById(R.id.reviewPicture);
         }
     }
 }
