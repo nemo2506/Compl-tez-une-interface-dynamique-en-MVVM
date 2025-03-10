79492181b85a49b2d114f7ea1a220ce9fe1f1633
MARC NAVARRO <navamarc@gmail.com>
2025-03-10 23:29:12 +0100

    Création package update et class reviewUpdate

---
+++ app/src/main/java/com/openclassrooms/tajmahal/update/reviewUpdate.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2025. Copyright © 2025 Taj Mahal. Tous droits réservés.
+ *
+ * Cette application, y compris son code source, sa conception, son contenu et ses fonctionnalités, est la propriété exclusive de Taj Mahal.
+ * Toute reproduction, distribution ou utilisation non autorisée, en tout ou en partie, est strictement interdite sans l'accord écrit préalable de Taj Mahal.
+ *
+ * Développé par Marc Navarro pour Taj Mahal.
+ */
+
+package com.openclassrooms.tajmahal.update;
+
+public class reviewUpdate {
+}
